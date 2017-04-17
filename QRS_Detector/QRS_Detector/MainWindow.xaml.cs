71309dbbc612fde7688b441c609102f9484cd6cd
Radosław Taborski <radoslaw.taborski@gmail.com>
2017-04-17 17:07:40 +0200

    dodano własny TitleBar

--- QRS_Detector/QRS_Detector/MainWindow.xaml.cs
+++ QRS_Detector/QRS_Detector/MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.IO;
 using System.Text;
 using WPF.Bootstrap.Controls;
 using System.Windows.Media;
+using System.Windows.Input;
 
 namespace QRS_Detector
 {
@@ -125,6 +126,64 @@ namespace QRS_Detector
             Chart1.Axes.Add(axisX);
             Chart1.Axes.Add(axisY);
             Chart1.LegendStyle = HideLegendStyle;
+        }
+
+        /// <summary>
+        /// TitleBar_MouseDown - Drag if single-click, resize if double-click
+        /// </summary>
+        private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                if (e.ClickCount == 2)
+                {
+                    AdjustWindowSize();
+                }
+                else
+                {
+                    Application.Current.MainWindow.DragMove();
+                }
+        }
+
+        /// <summary>
+        /// CloseButton_Clicked
+        /// </summary>
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        /// <summary>
+        /// MaximizedButton_Clicked
+        /// </summary>
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            AdjustWindowSize();
+        }
+
+        /// <summary>
+        /// Minimized Button_Clicked
+        /// </summary>
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        /// <summary>
+        /// Adjusts the WindowSize to correct parameters when Maximize button is clicked
+        /// </summary>
+        private void AdjustWindowSize()
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+                MaxButton.Content = "1";
+            }
+            else
+            {
+                this.WindowState = WindowState.Maximized;
+                MaxButton.Content = "2";
+            }
+
         }
     }
 }
