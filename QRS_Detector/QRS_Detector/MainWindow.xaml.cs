f7497a1d6dd0b9b7deaf13ac524fefcd87055daf
Radosław Taborski <radoslaw.taborski@gmail.com>
2017-04-17 19:28:59 +0200

    drobne poprawki- szablon gotowy

--- QRS_Detector/QRS_Detector/MainWindow.xaml.cs
+++ QRS_Detector/QRS_Detector/MainWindow.xaml.cs
@@ -130,9 +130,7 @@ namespace QRS_Detector
             Chart1.LegendStyle = HideLegendStyle;
         }
 
-        /// <summary>
-        /// TitleBar_MouseDown - Drag if single-click, resize if double-click
-        /// </summary>
+
         private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton == MouseButton.Left)
@@ -146,33 +144,25 @@ namespace QRS_Detector
                 }
         }
 
-        /// <summary>
-        /// CloseButton_Clicked
-        /// </summary>
+
         private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
         }
 
-        /// <summary>
-        /// MaximizedButton_Clicked
-        /// </summary>
+
         private void MaximizeButton_Click(object sender, RoutedEventArgs e)
         {
             AdjustWindowSize();
         }
 
-        /// <summary>
-        /// Minimized Button_Clicked
-        /// </summary>
+
         private void MinimizeButton_Click(object sender, RoutedEventArgs e)
         {
             this.WindowState = WindowState.Minimized;
         }
 
-        /// <summary>
-        /// Adjusts the WindowSize to correct parameters when Maximize button is clicked
-        /// </summary>
+
         private void AdjustWindowSize()
         {
             if (this.WindowState == WindowState.Maximized)
